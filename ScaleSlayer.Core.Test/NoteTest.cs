40e6316de2d60866a500d5048e7a654036db6c74
Koji Tilley <kojikun314@gmail.com>
2023-08-13 03:19:55 -0500

    Initial Note implementation with support for enharmonic comparisons and implicit string conversions

---
+++ ScaleSlayer.Core.Test/NoteTest.cs
@@ -0,0 +1,167 @@
+﻿
+namespace ScaleSlayer.Core.Test
+{
+    [TestClass]
+    public class NoteTest
+    {
+        [TestMethod]
+        public void Constructor()
+        {
+            // invoke parameterless constructor
+            var parameterless = new Note();
+            Assert.AreEqual('A', parameterless.Letter);
+            Assert.AreEqual(Accidental.Natural, parameterless.Accidental);
+            Assert.AreEqual(4, parameterless.Octave);
+            Assert.AreEqual(0, parameterless.Cents);
+
+            // check default args
+            var defaultArgsNote = new Note('C');
+            Assert.AreEqual('C', defaultArgsNote.Letter);
+            Assert.AreEqual(Accidental.Natural, defaultArgsNote.Accidental);
+            Assert.AreEqual(4, defaultArgsNote.Octave);
+            Assert.AreEqual(0, defaultArgsNote.Cents);
+
+            // use fully filled out constructor
+            var complexNote = new Note('G', Accidental.Sharp, 7, 13);
+            Assert.AreEqual('G', complexNote.Letter);
+            Assert.AreEqual(Accidental.Sharp, complexNote.Accidental);
+            Assert.AreEqual(7, complexNote.Octave);
+            Assert.AreEqual(13, complexNote.Cents);
+
+            // valid letters are converted to uppercase
+            Assert.AreEqual('A', new Note('A').Letter);
+            Assert.AreEqual('A', new Note('a').Letter);
+            Assert.AreEqual('B', new Note('B').Letter);
+            Assert.AreEqual('B', new Note('b').Letter);
+            Assert.AreEqual('C', new Note('C').Letter);
+            Assert.AreEqual('C', new Note('c').Letter);
+            Assert.AreEqual('D', new Note('D').Letter);
+            Assert.AreEqual('D', new Note('d').Letter);
+            Assert.AreEqual('E', new Note('E').Letter);
+            Assert.AreEqual('E', new Note('e').Letter);
+            Assert.AreEqual('F', new Note('F').Letter);
+            Assert.AreEqual('F', new Note('f').Letter);
+            Assert.AreEqual('G', new Note('G').Letter);
+            Assert.AreEqual('G', new Note('g').Letter);
+
+            // invalid letter throws a KeyNotFoundException
+            Assert.ThrowsException<KeyNotFoundException>(() => new Note('H'));
+        }
+
+        [TestMethod]
+        public void Enharmonic()
+        {
+            // enharmonic of basic natural note should be the same note
+            var A4 = new Note('A');
+            var A4_en = A4.Enharmonic();
+            Assert.AreEqual('A', A4_en.Letter);
+            Assert.AreEqual(Accidental.Natural, A4_en.Accidental);
+            Assert.AreEqual(4, A4_en.Octave);
+
+            // Enharmonic of a sharp should be flat
+            var Cs4 = new Note('C', Accidental.Sharp);
+            var Db4 = Cs4.Enharmonic();
+            Assert.AreEqual('D', Db4.Letter);
+            Assert.AreEqual(Accidental.Flat, Db4.Accidental);
+            Assert.AreEqual(4, Db4.Octave);
+
+            // Enharmonic of a flat should be a sharp
+            var Eb4 = new Note('E', Accidental.Flat);
+            var Ds4 = Eb4.Enharmonic();
+            Assert.AreEqual('D', Ds4.Letter);
+            Assert.AreEqual(Accidental.Sharp, Ds4.Accidental);
+            Assert.AreEqual(4, Ds4.Octave);
+
+            // Enharmonic of an enharmonic should be the original note
+            var Cs4_new = Db4.Enharmonic();
+            var Eb4_new = Ds4.Enharmonic();
+            Assert.AreEqual(Cs4, Cs4_new);
+            Assert.AreEqual(Eb4, Eb4_new);
+
+            // edge cases
+
+            // C natural is enharmonic to B sharp
+            // This should also drop the octave
+            var C4 = new Note('C');
+            var Bs3 = C4.Enharmonic();
+            Assert.AreEqual('B', Bs3.Letter);
+            Assert.AreEqual(Accidental.Sharp, Bs3.Accidental);
+            Assert.AreEqual(3, Bs3.Octave);
+
+            // Conversely, Bsharp is a C natural
+            Assert.AreEqual(C4, Bs3.Enharmonic());
+
+            // F flat is enharmonic to E natural
+            var Fb4 = new Note('F', Accidental.Flat);
+            var E4 = Fb4.Enharmonic();
+            Assert.AreEqual('E', E4.Letter);
+            Assert.AreEqual(Accidental.Natural, E4.Accidental);
+
+            // brute force every note-harmonic combination
+            // equality operator should compare enharmonics
+            Assert.AreEqual(new Note('A', Accidental.Sharp), new Note('B', Accidental.Flat));
+            Assert.AreEqual(new Note('B', Accidental.Sharp), new Note('C', Accidental.Natural, 5));
+            Assert.AreEqual(new Note('C', Accidental.Flat), new Note('B', Accidental.Natural, 3));
+            Assert.AreEqual(new Note('C', Accidental.Sharp), new Note('D', Accidental.Flat));
+            Assert.AreEqual(new Note('D', Accidental.Sharp), new Note('E', Accidental.Flat));
+            Assert.AreEqual(new Note('E', Accidental.Sharp), new Note('F', Accidental.Natural));
+            Assert.AreEqual(new Note('F', Accidental.Flat), new Note('E', Accidental.Natural));
+            Assert.AreEqual(new Note('F', Accidental.Sharp), new Note('G', Accidental.Flat));
+            Assert.AreEqual(new Note('G', Accidental.Sharp), new Note('A', Accidental.Flat));
+
+        }
+
+        [TestMethod]
+        public void ComparisonOperator_Equals()
+        {
+            // make two identical notes
+            var A4 = new Note('A');
+            var A4_explicit = new Note('A', Accidental.Natural, 4);
+            Assert.IsTrue(A4 == A4_explicit);
+            Assert.AreEqual(A4, A4_explicit);
+
+            // Notes of different cents are not equal
+            var A4_detuned = A4_explicit with { Cents = 7 };
+            Assert.IsFalse(A4 == A4_detuned);
+            Assert.IsTrue(A4 != A4_detuned);
+
+            // Notes of different octaves are not equal
+            var A5 = A4 with { Octave = 5 };
+            Assert.AreNotEqual(A4, A5);
+
+            // Notes of different letters are not equal
+            var B4 = new Note('B');
+            Assert.AreNotEqual(A4, B4);
+
+            // Enharmonics are treated as equal
+            var Ab4 = new Note('A', Accidental.Flat);
+            var Gs4 = new Note('G', Accidental.Sharp);
+            var Ab4_en = Ab4.Enharmonic();
+            Assert.IsTrue(Ab4 == Gs4);
+            Assert.AreEqual(Ab4, Gs4);
+            Assert.AreEqual(Ab4, Ab4.Enharmonic());
+        }
+
+        [TestMethod]
+        public void ImplicitString()
+        {
+            // construct A4 from string
+            Note A4 = "A";
+            Assert.AreEqual(new Note('A'), A4);
+
+            // Test all possible flats
+            Note Bb5 = new Note('B', Accidental.Flat, 5);
+            Assert.AreEqual(Bb5, "Bb5");
+            Assert.AreEqual(Bb5, " B b   5   ");    // whitespace agnostic regex
+            Assert.AreEqual(Bb5, "B flat 5");
+            Assert.AreEqual(Bb5, "bB5");            // case agnostic regex
+            Assert.AreEqual(Bb5, "B♭5");
+
+            // Test all possible sharps
+            Note Cs6 = new Note('C', Accidental.Sharp, 6);
+            Assert.AreEqual(Cs6, "C#6");
+            Assert.AreEqual(Cs6, "CSharp6");
+            Assert.AreEqual(Cs6, "C♯ 6");
+        }
+    }
+}
---
+++ ScaleSlayer.Core/Note.cs
@@ -0,0 +1,235 @@
+﻿using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
+
+namespace ScaleSlayer.Core
+{
+    /// <summary>
+    /// Represents a musical note
+    /// </summary>
+    public struct Note
+    {
+        public static readonly Dictionary<char, char> ValidLetters = new()
+        {
+            { 'A', 'A' },
+            { 'a', 'A' },
+            { 'B', 'B' },
+            { 'b', 'B' },
+            { 'C', 'C' },
+            { 'c', 'C' },
+            { 'D', 'D' },
+            { 'd', 'D' },
+            { 'E', 'E' },
+            { 'e', 'E' },
+            { 'F', 'F' },
+            { 'f', 'F' },
+            { 'G', 'G' },
+            { 'g', 'G' }
+        };
+
+        public static readonly Dictionary<string, Accidental> ValidAccidentals = new()
+        {
+            { "flat", Accidental.Flat },
+            { "b", Accidental.Flat },
+            { "♭", Accidental.Flat },
+            { "sharp", Accidental.Sharp },
+            { "#", Accidental.Sharp },
+            { "♯", Accidental.Sharp }
+        };
+
+        private static readonly double ratio_equalTemperament =
+            Math.Pow(2, 1 / 12);
+
+        /// <summary>
+        /// Regex used to convert a string representation of a note into a <see cref="Note"/>.
+        /// </summary>
+        private static readonly Regex NoteRegex =
+            new(@"^\s*(?<letter>[A-G])\s*(?<accidental>flat|b|sharp|\#|♭|♯)?\s*(?<octave>([1-9][0-9]+)|[0-9])?\s*$",
+                RegexOptions.IgnoreCase | RegexOptions.Compiled);
+
+        private char _letter;
+
+        /// <summary>
+        /// The Letter used to designate the note (A, B, C, D, E, F, G)
+        /// </summary>
+        /// <exception cref="KeyNotFoundException">Thrown when set letter not between A-G</exception>
+        public char Letter
+        {
+            get { return _letter; }
+            set { _letter = ValidLetters[value]; }
+        }
+
+        /// <summary>
+        /// Whether the note is a Flat, Sharp, or Natural
+        /// </summary>
+        public Accidental Accidental { get; set; }
+
+        /// <summary>
+        /// The number used to indicate the octave of the note.
+        /// </summary>
+        public int Octave { get; set; }
+
+        private int _cents;
+
+        /// <summary>
+        /// The percent of detuning between two semitones (100 cents = 1 semitone)
+        /// </summary>
+        public int Cents
+        {
+            get { return _cents; }
+            set
+            {
+                _cents = value;
+            }
+        }
+
+        /// <summary>
+        /// Instantiates a new <see cref="Note"/> object at A4.
+        /// </summary>
+        public Note() : this('A') { }
+
+        /// <summary>
+        /// Instantiates a new <see cref="Note"/> of a given <paramref name="letter"/>
+        /// </summary>
+        /// <param name="letter">The letter name used to designate the note</param>
+        /// <param name="accidental">Whether the note is flat, sharp, or natural</param>
+        /// <param name="octave">An integer denoting the octave of the note (Middle C is octave 4)</param>
+        /// <param name="cents">The amount of detuning applied to the note</param>
+        /// <exception cref="KeyNotFoundException">Thrown when set letter not between A-G</exception>
+        public Note(char letter, Accidental accidental = Accidental.Natural, int octave = 4, int cents = 0)
+        {
+            Letter = letter;
+            Accidental = accidental;
+            Octave = octave;
+            Cents = cents;
+        }
+
+        /// <summary>
+        /// Implicitly converts a string representation of a note into a <see cref="Note"/>
+        /// </summary>
+        /// <param name="note">The string representation of a note</param>
+        public static implicit operator Note(string note)
+        {
+            // match string with regex
+            var match = NoteRegex.Match(note);
+            if (!match.Success)
+            {
+                throw new ArgumentException("Note string could not be parsed into a valid Note", "note");
+            }
+
+            // get regex match groups
+            var groups = match.Groups;
+            var letter = char.Parse(groups["letter"].Value);
+            var accidental = groups["accidental"].Success ? ValidAccidentals[groups["accidental"].Value.ToLower()] : Accidental.Natural;
+            var octave = groups["octave"].Success ? int.Parse(groups["octave"].Value) : 4;
+
+            // construct new note object
+            return new Note(letter, accidental, octave);
+        }
+
+        public Note Enharmonic() => Accidental switch
+        {
+            Accidental.Natural => Letter switch
+            {
+                'B' => this with { Letter = 'C', Accidental = Accidental.Flat, Octave = Octave + 1 },
+                'C' => this with { Letter = 'B', Accidental = Accidental.Sharp, Octave = Octave - 1 },
+                'E' => this with { Letter = 'F', Accidental = Accidental.Flat },
+                'F' => this with { Letter = 'E', Accidental = Accidental.Sharp },
+                _ => this
+            },
+            Accidental.Sharp => Letter switch
+            {
+                'B' => this with { Letter = 'C', Accidental = Accidental.Natural, Octave = Octave + 1 },
+                'E' => this with { Letter = 'F', Accidental = Accidental.Natural },
+                'G' => this with { Letter = 'A', Accidental = Accidental.Flat },
+                _ => this with { Letter = (char)(Letter + 1), Accidental = Accidental.Flat }
+            },
+            Accidental.Flat => Letter switch
+            {
+                'A' => this with { Letter = 'G', Accidental = Accidental.Sharp },
+                'C' => this with { Letter = 'B', Accidental = Accidental.Natural, Octave = Octave - 1 },
+                'F' => this with { Letter = 'E', Accidental = Accidental.Natural },
+                _ => this with { Letter = (char)(Letter - 1), Accidental = Accidental.Sharp }
+            },
+            _ => throw new InvalidEnumArgumentException("Property \"Accidental\" has a non-standard enum value.")
+        };
+
+        public override bool Equals([NotNullWhen(true)] object? obj)
+        {
+            // force usage of equality operator if other object is a Note
+            // default equality for structs compares member data, which can be different for enharmonics
+            if (obj is Note note)
+                return this == note;
+
+            return base.Equals(obj);
+        }
+
+        public static bool operator ==(Note left, Note right)
+        {
+            var enharmonic = right.Enharmonic();
+
+            if (left.Cents == right.Cents)
+            {
+                if (left.Octave == right.Octave || left.Octave == enharmonic.Octave)
+                {
+                    // notes have same cents and octave, just check letter and accidental
+                    if ((left.Letter == right.Letter && left.Accidental == right.Accidental) ||
+                        (left.Letter == enharmonic.Letter && left.Accidental == enharmonic.Accidental))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public static bool operator !=(Note left, Note right) => !(left == right);
+
+        ///// <summary>
+        ///// Returns a new <see cref="Note"/> that's offset by a certain number of <paramref name="semitones"/>.
+        ///// </summary>
+        ///// <param name="note"></param>
+        ///// <param name="semitones"></param>
+        ///// <returns></returns>
+        //public static Note operator +(Note note, int semitones)
+        //{
+        //    int newOctave = note.Octave + (semitones / 12);
+        //    return new 
+        //}
+
+        ///// <summary>
+        ///// Implicitly converts a <see cref="Note"/> into its frequency value in Hertz (Hz) using equal temperment
+        ///// </summary>
+        ///// <param name="note">The <see cref="Note"/> object to convert into Hz</param>
+        //public static implicit operator double(Note note)
+        //{
+        //    const double standardTuning = 440.0;
+        //
+        //}
+        //
+        //public static bool operator >(Note left, Note right)
+        //{
+        //    if (left.Octave != right.Octave)
+        //    {
+        //        return left.Octave > right.Octave;
+        //    }
+        //    else
+        //    {
+        //        if 
+        //    }
+        //}
+        //
+        //public static (int Semitones, int Cents) operator -(Note left, Note right)
+        //{
+        //
+        //}
+    }
+
+    public enum Accidental
+    {
+        Natural,
+        Flat,
+        Sharp
+    }
+}
