3dc276710ef80dbe5fc8b8be4da59fed12b44b49
Koji Tilley <kojikun314@gmail.com>
2023-09-01 18:47:42 -0500

    Added more extension methods to results of scale generation

---
+++ ScaleSlayer.Core.Test/ScaleExtensionsTest.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScaleSlayer.Core.Test
+{
+    [TestClass]
+    public class ScaleExtensionsTest
+    {
+        [TestMethod]
+        public void OfAccidental()
+        {
+            var AMajorScale = Scales.Major.Generate("A4").Take(8);
+            Assert.AreEqual("ABCDEFGA", AMajorScale.Letters());
+
+            // Force usage of flats
+            Assert.AreEqual("ABDDEGAA", AMajorScale.OfAccidental(Accidental.Flat).Letters());
+
+            // Scale that only uses sharps should not change
+            Assert.AreEqual(AMajorScale.Letters(), AMajorScale.OfAccidental(Accidental.Sharp).Letters());
+
+            // Forcing accidentals would use strange enharmonics, such as B Sharp, C Flat, E Sharp, F Flat
+            var CMajorScale = Scales.Major.Generate("C4").Take(8);
+            Assert.AreEqual("CDEFGABC", CMajorScale.Letters());
+
+            Assert.AreEqual("CDEFGABC", CMajorScale.OfAccidental(Accidental.Flat).Letters());
+            Assert.AreEqual("CDFFGACC", CMajorScale.OfAccidental(Accidental.Flat, convertNaturals: true).Letters());
+
+            Assert.AreEqual("CDEFGABC", CMajorScale.OfAccidental(Accidental.Sharp).Letters());
+            Assert.AreEqual("BDEEGABB", CMajorScale.OfAccidental(Accidental.Sharp, convertNaturals: true).Letters());
+
+        }
+
+        [TestMethod]
+        public void WithinRange()
+        {
+            var CMajorScale = Scales.Major.Generate("C");
+
+            // Get the notes that are bound by the fifth
+            Assert.AreEqual("CDEF", CMajorScale.WithinRange((int)Interval.Fifth).Letters());
+
+            // Include the fifth
+            Assert.AreEqual("CDEFG", CMajorScale.WithinRange((int)Interval.Fifth, exclusive: false).Letters());
+
+            // Get an octave
+            Assert.AreEqual("CDEFGAB", CMajorScale.WithinRange((int)Interval.Octave).Letters());
+
+            // Range should be agnostic of ordering of notes
+            Assert.AreEqual("CBAGFED", CMajorScale.Take(15).Reverse().WithinRange((int)Interval.Octave).Letters());
+
+            // Range of 0 should return an empty enumerable if exclusive
+            Assert.AreEqual("", CMajorScale.WithinRange(0).Letters());
+
+            // If inclusive, a range of 0 should return the root
+            Assert.AreEqual("C", CMajorScale.WithinRange(0, exclusive: false).Letters());
+        }
+
+        [TestMethod]
+        public void Octaves()
+        {
+            var root = new Note('C');
+            var CMajorScale = Scales.Major.Generate(root);
+
+            // Generate 1 octave
+            Assert.AreEqual("CDEFGABC", CMajorScale.Octaves(1).Letters());
+            Assert.AreEqual("CDEFGAB", CMajorScale.Octaves(1, containsLastRoot: false).Letters());
+
+            // Generate 2 octaves
+            Assert.AreEqual("CDEFGABCDEFGABC", CMajorScale.Octaves(2).Letters());
+            Assert.AreEqual("CDEFGABCDEFGAB", CMajorScale.Octaves(2, containsLastRoot: false).Letters());
+
+            // Calculate far ahead
+            int octaves = 10;
+            Assert.AreEqual(root with { Octave = root.Octave + octaves }, CMajorScale.Octaves(octaves).Last());
+
+            // 0 should return just the root (or empty if root not contained)
+            Assert.AreEqual("C", CMajorScale.Octaves(0).Letters());
+            Assert.AreEqual("", CMajorScale.Octaves(0, containsLastRoot: false).Letters());
+
+        }
+    }
+}
---
+++ ScaleSlayer.Core/ScaleExtensions.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScaleSlayer.Core
+{
+    /// <summary>
+    /// Extension methods typically used on the result of a <see cref="Scale.Generate"/>
+    /// </summary>
+    public static class ScaleExtensions
+    {
+        /// <summary>
+        /// Forces the usage of an <paramref name="accidental"/> for a set of generated notes.
+        /// </summary>
+        /// <param name="source">A list of <see cref="Note"/>s, typically as a result of a call to <see cref="Scale.Generate"/></param>
+        /// <param name="accidental">The <see cref="Accidental"/> to convert the current note to</param>
+        /// <param name="convertNaturals">Whether to force naturals to use strange enharmonics such as B sharp or C flat</param>
+        /// <returns>An <see cref="IEnumerable{Note}"/> of notes with enharmonics that represent the supplied <paramref name="accidental"/></returns>
+        public static IEnumerable<Note> OfAccidental(this IEnumerable<Note> source, Accidental accidental, bool convertNaturals = false)
+        {
+            foreach (var note in source)
+            {
+                yield return (note.Accidental == accidental) switch
+                {
+                    // yield unmodified note if the accidental is of the correct type
+                    true => note,
+                    false => note.Accidental switch
+                    {
+                        // if current note is a natural, only yield its enharmonic if
+                        // option is set to do so
+                        Accidental.Natural => 
+                            convertNaturals
+                            ? (note.Enharmonic().Accidental == accidental ? note.Enharmonic() : note)
+                            : note,
+                        // note of opposite accidental; yield enharmonic
+                        _ => note.Enharmonic()
+                    }
+                };
+            }
+        }
+
+        /// <summary>
+        /// Returns a list of notes constrained within a given number of semitones.
+        /// </summary>
+        /// <param name="source">A list of <see cref="Note"/>s, typically as a result of a call to <see cref="Scale.Generate"/></param>
+        /// <param name="semitones">The number of semitones to constrain the <paramref name="source"/> list</param>
+        /// <param name="exclusive">Whether the end of the range is inclusive (false) or exclusive (true)</param>
+        /// <returns>Returns a finite <see cref="IEnumerable{Note}"/> of notes constrained by the number of <paramref name="semitones"/> given.</returns>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="semitones"/> is less than 0.</exception>
+        public static IEnumerable<Note> WithinRange(this IEnumerable<Note> source, int semitones, bool exclusive = true)
+        {
+            if (semitones < 0)
+                throw new ArgumentException($"\"{nameof(semitones)}\" cannot be less than 0.", nameof(semitones));
+
+            // get first note in sequence
+            var root = source.FirstOrDefault();
+            semitones += exclusive ? 0 : 1;
+
+            foreach (var note in source)
+            {
+                // yield current note if distance between current note and root within range
+                if (Math.Abs(note - root) < semitones)
+                {
+                    yield return note;
+                }
+                else
+                {
+                    yield break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns a list of notes constrained within a given number of octaves.
+        /// </summary>
+        /// <param name="source">A list of <see cref="Note"/>s, typically as a result of a call to <see cref="Scale.Generate"/></param>
+        /// <param name="count">The number of octaves to constrain the <paramref name="source"/> list</param>
+        /// <param name="containsLastRoot">Whether or not to include the note that bounds the last octave</param>
+        /// <returns>Returns a finite <see cref="IEnumerable{Note}"/> of notes constrained by the number of octaves given.</returns>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="count"/> is less than 0.</exception>
+        public static IEnumerable<Note> Octaves(this IEnumerable<Note> source, int count, bool containsLastRoot = true)
+        {
+            if (count < 0)
+                throw new ArgumentException($"\"{nameof(count)}\" cannot be less than 0.", nameof(count));
+
+            // an octave is 12 semitones
+            return source.WithinRange(count * 12, !containsLastRoot);
+        }
+    }
+}
